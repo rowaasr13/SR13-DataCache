1dbc0278e96c42c92a7516143e80bffa85e36750
(rowaasr13) Oleg V. Volkov <rowaasr13@gmail.com>
2021-04-06 19:47:39 +0300

    Maw Buffs (Torghast Anima Powers) cache

---
+++ MawBuffs.lua
@@ -0,0 +1,40 @@
+local a_name, a_env = ...
+
+-- [AUTOLOCAL START]
+local CreateFrame = CreateFrame
+local IsInJailersTower = IsInJailersTower
+local UnitAura = UnitAura
+local wipe = wipe
+-- [AUTOLOCAL END]
+
+local active_maw_buffs = {}
+a_env.a_export.active_talents = active_maw_buffs
+
+local function active_maw_buffs_update()
+   for idx = 1, 200 do
+      local _, icon, count, _, _, _, _, _, _, spellID = UnitAura("player", idx, "MAW")
+      if spellID then
+         active_maw_buffs[spellID] = count
+      else
+         break -- watch out if early break always work correctly, Blizzard doesn't use it
+      end
+   end
+end
+
+local frame = CreateFrame("Frame")
+frame:SetScript("OnEvent", function(self, event, ...)
+   if (event == "PLAYER_ENTERING_WORLD") then
+      wipe(active_maw_buffs)
+      if IsInJailersTower() then
+         active_maw_buffs_update()
+         frame:RegisterUnitEvent("UNIT_AURA", "player")
+      else
+         frame:UnregisterEvent("UNIT_AURA")
+      end
+   elseif (event == "UNIT_AURA") then
+      wipe(active_maw_buffs)
+      active_maw_buffs_update()
+   end
+
+end)
+frame:RegisterEvent("PLAYER_ENTERING_WORLD")
